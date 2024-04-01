05e8bc266f20aa723786bcf8274767d78dc61a3f
Ayaan Siddiqui <160242936+MuhammadAyaanSiddiqui@users.noreply.github.com>
2024-04-01 12:41:19 +0500

    Add files via upload

---
+++ Nice.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+ import inquirer from "inquirer";
+import { isAwaitExpression } from "typescript";
+
+const answer = await inquirer.prompt([
+  { message: " Enter First Number ", type: "number", name: "FirstNumber" },
+  { message: " Enter Second Number ", type: "number", name: "SecondNumber" },
+  {
+    message: "Select one of the operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+// conditional answer
+
+if (answer.operator === "Addition") {
+  console.log(answer.FirstNumber + answer.SecondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.FirstNumber - answer.SecondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.FirstNumber * answer.SecondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.FirstNumber / answer.SecondNumber);
+} else {
+  console.log("Please select a valid operator");
+}
